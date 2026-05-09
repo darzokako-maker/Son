0c0c005719e802975d8ee24abf58fee4cfe7c52e
darzokako-maker <darzokako@gmail.com>
2026-05-09 12:48:48 +0300

    Add resource definitions for menu and files

---
+++ resource.h
@@ -0,0 +1,10 @@
+#define IDR_DLL_FILE 101
+#define IDR_SYS_FILE 102
+#define IDM_MAIN_MENU 200
+
+// Menü Elemanları
+#define IDM_STATUS 301
+#define IDM_BYPASS_MODE 302
+#define IDM_RESET 303
+#define IDM_EXIT 304
+
